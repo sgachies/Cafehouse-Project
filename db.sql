a01c8c0d48c275512c904eec456524526c10525e
stevexdev <ngechu.gachie@gmail.com>
2015-04-28 20:31:26 +0000

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.0.10.7
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 28, 2015 at 05:14 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: Apr 28, 2015 at 04:29 PM
+-- Server version: 5.5.42-cll
+-- PHP Version: 5.4.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,30 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ussd`
+-- Database: `javapals_bima`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `points`
---
-
-CREATE TABLE IF NOT EXISTS `points` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `phone_number` varchar(50) NOT NULL,
-  `counter` varchar(50) NOT NULL,
-  `mpesa_code` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `points`
---
-
-INSERT INTO `points` (`id`, `phone_number`, `counter`, `mpesa_code`) VALUES
-(3, '254723401197', '1', 'BI55EQ861'),
-(4, '254726371675', '1', 'BI55EQ861');
 
 -- --------------------------------------------------------
 
@@ -54,28 +32,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `full_name` varchar(50) NOT NULL,
   `national_id` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `phone_number`, `full_name`, `national_id`) VALUES
-(1, '254723401197', 'stevebab1', '27816668'),
-(2, '254726371675', 'stevebab2', '27816669');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ussd_logs`
+-- Table structure for table `user_points`
 --
 
-CREATE TABLE IF NOT EXISTS `ussd_logs` (
+CREATE TABLE IF NOT EXISTS `user_points` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `phone` varchar(50) NOT NULL,
-  `text` varchar(50) NOT NULL,
+  `phone_number` varchar(50) NOT NULL,
+  `counter` varchar(50) NOT NULL,
+  `mpesa_code` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=245 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
