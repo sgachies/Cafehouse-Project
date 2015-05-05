12af0bf6a6f4e9e901d77a030312b9f3fe7d6e64
stevexdev <ngechu.gachie@gmail.com>
2015-05-05 15:28:52 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 28, 2015 at 04:29 PM
+-- Generation Time: May 05, 2015 at 08:18 AM
 -- Server version: 5.5.42-cll
 -- PHP Version: 5.4.23
 
@@ -17,8 +17,104 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `javapals_bima`
+-- Database: `javapals_cafehouse`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE IF NOT EXISTS `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu` varchar(50) NOT NULL,
+  `price` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `menu`, `price`) VALUES
+(1, 'Cafe Mocha', '150'),
+(2, 'Caffe Latte', '180'),
+(3, 'Coffee Milk', '230'),
+(4, 'Americano Coffee', '150'),
+(5, 'Black Tea', '210'),
+(6, 'White Tea', '110'),
+(7, 'Green Tea', '100'),
+(8, 'Pizza small + 1 liter soda', '800'),
+(9, 'Pizza Medium  + 2 liter Soda', '1000'),
+(10, 'Pizza full + 2 liter Soda', '1200');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone_number` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `ref_code` varchar(60) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `product` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `qty` varchar(10) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `status` varchar(10) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `date` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesapi_account`
+--
+
+CREATE TABLE IF NOT EXISTS `pesapi_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `identifier` varchar(255) NOT NULL,
+  `push_in` tinyint(1) NOT NULL,
+  `push_out` tinyint(1) NOT NULL,
+  `push_neutral` tinyint(1) NOT NULL,
+  `settings` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type_index` (`type`),
+  KEY `definedby` (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesapi_payment`
+--
+
+CREATE TABLE IF NOT EXISTS `pesapi_payment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `super_type` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `receipt` varchar(255) NOT NULL,
+  `time` datetime NOT NULL,
+  `phonenumber` varchar(45) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `account` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `amount` bigint(20) NOT NULL,
+  `post_balance` bigint(20) NOT NULL,
+  `note` varchar(255) NOT NULL,
+  `transaction_cost` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type_index` (`type`),
+  KEY `name_index` (`name`),
+  KEY `phone_index` (`phonenumber`),
+  KEY `time_index` (`time`),
+  KEY `super_index` (`super_type`),
+  KEY `fk_mpesapi_payment_account_idx` (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -32,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `full_name` varchar(50) NOT NULL,
   `national_id` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -43,10 +139,21 @@ CREATE TABLE IF NOT EXISTS `users` (
 CREATE TABLE IF NOT EXISTS `user_points` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `phone_number` varchar(50) NOT NULL,
-  `counter` varchar(50) NOT NULL,
+  `national_id` varchar(20) NOT NULL,
+  `points` varchar(50) NOT NULL,
   `mpesa_code` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pesapi_payment`
+--
+ALTER TABLE `pesapi_payment`
+  ADD CONSTRAINT `fk_mpesapi_payment_account` FOREIGN KEY (`account_id`) REFERENCES `pesapi_account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
